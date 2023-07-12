cbdf855e4eb311958746cdc11a5713680164d5e2
David Zbarsky <dzbarsky@gmail.com>
2023-07-11 21:48:46 -0400

    [kms] Implement ReEncrypt

--- services/kms/http.go
+++ services/kms/http.go
@@ -17,6 +17,7 @@ func (k *KMS) RegisterHTTPHandlers(methodRegistry http.Registry) {
 	http.Register(methodRegistry, service, "ListAliases", k.ListAliases)
 	http.Register(methodRegistry, service, "ListKeys", k.ListKeys)
 	http.Register(methodRegistry, service, "ListResourceTags", k.ListResourceTags)
+	http.Register(methodRegistry, service, "ReEncrypt", k.ReEncrypt)
 	http.Register(methodRegistry, service, "TagResource", k.TagResource)
 	http.Register(methodRegistry, service, "UntagResource", k.UntagResource)
 }

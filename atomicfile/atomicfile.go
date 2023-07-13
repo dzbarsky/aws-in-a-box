04352ad46aca650dcca5c628336ba011fd8f0f6a
David Zbarsky <david.zbarsky@airtable.com>
2023-07-13 11:45:23 -0400

    [kms] Support persisting keys to disk

---
+++ atomicfile/atomicfile.go
@@ -0,0 +1,46 @@
+package atomicfile
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+// Write ensures that the data in `bytes` is either written fully to filename, or errors
+func Write(filename string, data []byte, perm os.FileMode) (err error) {
+	fi, err := os.Stat(filename)
+	if err == nil && !fi.Mode().IsRegular() {
+		return fmt.Errorf("%s already exists and is not a regular file", filename)
+	}
+
+	// tempfile must be in the same volume to ensure rename is atomic!
+	f, err := os.CreateTemp(filepath.Dir(filename), filepath.Base(filename)+".tmp")
+	if err != nil {
+		return err
+	}
+	tmpName := f.Name()
+	defer func() {
+		if err != nil {
+			f.Close()
+			os.Remove(tmpName)
+		}
+	}()
+	if _, err := f.Write(data); err != nil {
+		return err
+	}
+	if runtime.GOOS != "windows" {
+		if err := f.Chmod(perm); err != nil {
+			return err
+		}
+	}
+	// Need an fsync; otherwise it's valid for filesystems to end up with a
+	// 0-length file post-rename.
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+	return os.Rename(tmpName, filename)
+}

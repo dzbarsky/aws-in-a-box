f1fc6c6861c078d536eed53801a50c61890d9bee
David Zbarsky <dzbarsky@gmail.com>
2023-09-16 15:17:31 -0400

    Prepare source archive releases

---
+++ .github/workflows/build_release.sh
@@ -0,0 +1,15 @@
+set -eux
+
+for GOOS in "windows" "linux" "darwin"; do
+    for GOARCH in "amd64" "arm64"; do
+        CGO_ENABLED=0 go build -ldflags "-w" -trimpath -o "./aws-in-a-box-$GOOS-$GOARCH"
+    done
+done
+
+# Set by GH actions, see
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+TAG=${GITHUB_REF_NAME}
+# The prefix is chosen to match what GitHub generates for source archives
+PREFIX="aws-in-a-box-${TAG:1}"
+ARCHIVE="aws-in-a-box-$TAG.tar.gz"
+git archive --format=tar --prefix="${PREFIX}/" "${TAG}" | gzip > "$ARCHIVE"
